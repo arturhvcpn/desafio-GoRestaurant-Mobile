de2ce90df35c29e71e20da4792f0e42092a29d44
Artur <arturcastropolonorte@gmail.com>
2020-08-13 18:37:55 -0300

    challenge success

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,7 +32,10 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const response = await api.get('/favorites');
+      const favoritesResponse = response.data;
+
+      setFavorites(favoritesResponse);
     }
 
     loadFavorites();
